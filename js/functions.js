57525c242a567eca3cd8de5d3d85dc53b2b07825
Romerito da Silva de Abreu <romero.ito@gmail.com>
2021-08-10 17:24:08 -0300

    Subindo ajustes no projeto

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,18 @@ const items = [
     img: "../images/image.jpg",
     quantidade: 0
   },
+  {
+    id: 1,
+    nome: "short",
+    img: "../images/image.jpg",
+    quantidade: 0
+  },
+  {
+    id: 2,
+    nome: "sapato",
+    img: "../images/image.jpg",
+    quantidade: 0
+  }
 ]
 
 inicializarLoja = () => {
@@ -15,3 +27,24 @@ inicializarLoja = () => {
 }
 
 inicializarLoja();
+
+atualizarCarrinho = () => {
+  var containerCarrinho = document.getElementById("carrinho");
+  containerCarrinho.innerHTML = "";
+  items.map((val) => {
+    if (val.quantidade > 0) {
+      containerCarrinho.innerHTML += '<p>'+ val.nome +' | quantidade: '+ val.quantidade +'</p><hr>';
+    }
+  })
+}
+
+var links = document.getElementsByTagName("a");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    let key = this.getAttribute("key");
+    items[key].quantidade++;
+    atualizarCarrinho();
+    return false;
+  })
+}
